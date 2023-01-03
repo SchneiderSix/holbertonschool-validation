11c86c8fc87057a987810721ff150082dbbc83c3
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2023-01-03 14:53:02 -0300

    Fix setup last check

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -1,8 +1,3 @@
 sudo apt-get update && sudo apt-get install -y git hugo make curl build-essential
 hugo new site dist
-cd dist
-git init
-git submodule add https://github.com/theNewDynamic/gohugo-theme-ananke themes/ananke
-echo "theme = 'ananke'" >> config.toml
-hugo new posts/my-first-post.md
 hugo
