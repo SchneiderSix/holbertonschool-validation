f306a869093625eb42da0153e7e4159902a72e25
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2023-01-03 14:54:10 -0300

    Fix setup last check

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -1,3 +1,3 @@
-sudo apt-get update && sudo apt-get install -y git hugo make curl build-essential
+sudo apt-get update && sudo apt-get install -y hugo make curl build-essential
 hugo new site dist
 hugo
