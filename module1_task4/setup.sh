8ee15e267fd9424337cfd8ac087ab90a3bd9ac55
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2023-01-06 17:07:22 -0300

    wget method install and clean with dpkg

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -1,7 +1,5 @@
-sudo apt-get update && sudo apt-get install -y make curl build-essential
-h=${curl -s 'https://api.github.com/repositories/11180687/relases/latest' | grep -o 'https://.*hugo_extended.*_Linux-64bit.tar.gz'}
-curl -s $h -L -o hugo.tar.gz
-tar -fxz hugo.tar.gz -C /usr/local/bin
-rm hugo.tar.gz /usr/local/bin/LICENSE /usr/local/bin/README.md
+sudo apt-get update && sudo apt-get install -y make wget
+wget https://github.com/gohugoio/hugo/relases/download/v0.79.0/hugo_0.79.0_Linux-64bit.deb
+dpkg -i hugo_0.79.0_Linux-64bit.deb
 make build
 exit 0
