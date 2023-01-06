724923e95099761dd65d88a7b2ca73ac96ae6e83
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2023-01-06 16:43:34 -0300

    Update setup

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -1,2 +1,7 @@
-sudo apt-get update && sudo apt-get install -y hugo make curl build-essential
-hugo new site dist
+sudo apt-get update && sudo apt-get install -y make curl build-essential
+h=${grep -o 'https://.*hugo_extended.*_Linux-64bit.tar.gz'}
+curl -s $h -L -o hugo.tar.gz
+tar -fxz hugo.tar.gz -C /usr/local/bin
+rm hugo.tar.gz /usr/local/bin/LICENSE /usr/local/bin/README.md
+make build
+exit 0
