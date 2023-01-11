569a79b837c18754be3f10074aec45862eede820
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2023-01-11 17:00:06 -0300

    Remove make build from setup

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -2,5 +2,4 @@ sudo apt-get update && sudo apt-get install -y make wget npm
 wget https://github.com/gohugoio/hugo/releases/download/v0.79.0/hugo_0.79.0_Linux-64bit.deb
 dpkg -i hugo_0.79.0_Linux-64bit.deb
 npm install -g markdownlint-cli
-make build
 exit 0
