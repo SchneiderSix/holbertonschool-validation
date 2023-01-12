34079ae79e4b7a24ad36379a825913f8e6b2bd88
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2023-01-12 19:28:22 -0300

    Update setup.sh

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -1,4 +1,3 @@
-rm -fr /usr/local/go/bin/go
 apt-get update && apt-get install -y make wget npm zip
 wget https://github.com/gohugoio/hugo/releases/download/v0.79.0/hugo_0.79.0_Linux-64bit.deb
 dpkg -i hugo_0.79.0_Linux-64bit.deb
