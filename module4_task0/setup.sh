b702bf89a3c5a71c14f024146ebaf702b07b69c8
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2023-01-12 11:03:33 -0300

    delete make build in setup

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -2,5 +2,4 @@ apt-get update && apt-get install -y make wget npm zip
 wget https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb
 dpkg -i hugo_0.79.0_Linux-64bit.deb
 npm install -g markdownlint-cli
-make build
 exit 0
