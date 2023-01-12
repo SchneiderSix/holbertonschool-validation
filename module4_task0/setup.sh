ba2353017d55393b9fac9b3ed3176c9cef507411
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2023-01-12 18:33:36 -0300

    Old rm dir go

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -3,4 +3,6 @@ apt-get update && apt-get install -y make wget npm zip
 wget https://github.com/gohugoio/hugo/releases/download/v0.79.0/hugo_0.79.0_Linux-64bit.deb
 dpkg -i hugo_0.79.0_Linux-64bit.deb
 npm install -g markdownlint-cli
+which go
+sudo rm -rvf /usr/local/go/
 exit 0
