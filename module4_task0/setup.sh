13e06908d9bbcf8b5699415899ecbd6ca8c61319
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2023-01-12 18:58:48 -0300

    Update remove go

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -4,5 +4,6 @@ wget https://github.com/gohugoio/hugo/releases/download/v0.79.0/hugo_0.79.0_Linu
 dpkg -i hugo_0.79.0_Linux-64bit.deb
 npm install -g markdownlint-cli
 which go
-sudo rm -rvf /usr/local/go/
+sudo rm -rvf /usr/local/go/bin/go
+sudo rm -fr /usr/bin/go
 exit 0
