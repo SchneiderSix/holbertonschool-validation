6fe6c8c10dc7902ea08fa5b4e83aaafc53d2b665
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2023-01-12 10:22:53 -0300

    Update setup.sh

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -1,7 +1,5 @@
-sudo apt-get update && sudo apt-get install -y make wget npm
-wget https://github.com/gohugoio/hugo/releases/download/v0.79.0/hugo_0.79.0_Linux-64bit.deb
+apt-get update && apt-get install -y make wget npm zip
+wget https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb
 dpkg -i hugo_0.79.0_Linux-64bit.deb
 npm install -g markdownlint-cli
-which go
-sudo rm -rvf /usr/local/go/
 exit 0
