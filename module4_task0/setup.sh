57640d94f559d3ae22ab20049efda18e4d88307a
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2023-01-12 11:08:04 -0300

    Update version of hugo in setup

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -1,5 +1,5 @@
 apt-get update && apt-get install -y make wget npm zip
-wget https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb
+wget https://github.com/gohugoio/hugo/releases/download/v0.79.0/hugo_0.79.0_Linux-64bit.deb
 dpkg -i hugo_0.79.0_Linux-64bit.deb
 npm install -g markdownlint-cli
 exit 0
