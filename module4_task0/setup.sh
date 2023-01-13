81d756316226ec0a3b2e8c5836cfc9778f8db4b5
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2023-01-12 22:29:55 -0300

    Update setup.sh

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -1,9 +1,10 @@
-apt-get update && apt-get install -y make wget npm zip
+apt-get update && apt-get install -y make wget npm zip git
 wget https://github.com/gohugoio/hugo/releases/download/v0.79.0/hugo_0.79.0_Linux-64bit.deb
 dpkg -i hugo_0.79.0_Linux-64bit.deb
 npm install -g markdownlint-cli
 which go
 sudo rm -rvf /usr/local/go/bin/go
 sudo rm -fr /usr/bin/go
+git clone https://github.com/SchneiderSix/holbertonschool-validation.git
 ls
 exit 0
